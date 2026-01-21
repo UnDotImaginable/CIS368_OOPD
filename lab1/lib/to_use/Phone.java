cc1d53a69080dfec917b5a7d424a20687f8b1ed8
Veer Gaudani <veergaudani@gmail.com>
2026-01-21 10:13:59 -0500

    Finished address and phone

---
+++ lab1/lib/to_use/Phone.java
@@ -0,0 +1,111 @@
+public class Phone {
+    private String areaCode;
+    private String exchange;
+    private String extension;
+
+    private boolean checkAreaCode(String code) {
+
+        if (code.length() != 3)
+            return false;
+        
+        for (int i = 0; i < code.length(); i++) {
+            char c = code.charAt(i);
+
+            if (!Character.isDigit(c)) {
+                return false;
+            }
+
+        }
+
+        return true;
+    }
+
+    private boolean checkExchange(String exc) {
+
+        if (exc.length() != 3)
+            return false;
+        
+        for (int i = 0; i < exc.length(); i++) {
+            char c = exc.charAt(i);
+
+            if (!Character.isDigit(c)) {
+                return false;
+            }
+
+        }
+
+        return true;
+    }
+
+    private boolean checkExtension(String ext) {
+
+        if (ext.length() != 4)
+            return false;
+        
+        for (int i = 0; i < ext.length(); i++) {
+            char c = ext.charAt(i);
+
+            if (!Character.isDigit(c)) {
+                return false;
+            }
+
+        }
+
+        return true;
+    }
+
+
+
+    Phone() {
+        
+    }
+
+    Phone(String areaCode, String exchange, String extension) {
+        if (!checkAreaCode(areaCode))
+            throw new IllegalArgumentException("Invalid area code!");
+        
+        if (!checkExchange(exchange))
+            throw new IllegalArgumentException("Invalid exchange!");
+        
+        if (!checkExtension(extension))
+            throw new IllegalArgumentException("Invalid extension!");
+
+        this.areaCode = areaCode;
+        this.exchange = exchange;
+        this.extension = extension;
+    }
+
+
+    public String getAreaCode() {
+        return areaCode;
+    }
+
+    public void setAreaCode(String newAreaCode) {
+        if (!checkAreaCode(newAreaCode))
+            throw new IllegalArgumentException("Invalid area code!");
+    
+        this.areaCode = newAreaCode;
+    }
+
+    public String getExchange() {
+        return exchange;
+    }
+
+    public void setExchange(String newExchange) {
+        if (!checkExchange(newExchange))
+            throw new IllegalArgumentException("Invalid exchange!");
+
+        this.areaCode = newExchange;
+    }
+
+    public String getExtension() {
+        return extension;
+    }
+
+    public void setExtension(String newExtension) {
+        if (!checkExtension(newExtension))
+            throw new IllegalArgumentException("Invalid extension!");        
+
+        this.extension = newExtension;
+    }
+}
